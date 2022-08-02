d812659ffb3bfb411c919cba63db137b65bc4af1
pruthvipheonix <98202345+pruthvipheonix@users.noreply.github.com>
2022-08-03 00:00:20 +0530

    Updating AUtomation.sh with TASK3
    Adding
    1) BookKeeping or Inventory Logic
    2) Cron Job scheduler

--- automation.sh
+++ automation.sh
@@ -68,11 +68,6 @@ archive_logs(){
 
 
 ## Copying the Archive logs to S3 Bucket using Amazon CLI
-## Install AWS CLI before copying to s3 bucket
- 
-#sudo apt update
-#sudo apt install awscli
-
 copy_archive_logs(){
         echo "Copying to S3 Bucket started";
         bucketName=$1
@@ -80,11 +75,63 @@ copy_archive_logs(){
 }
 
 
+
+## Bookkeeping
+
+inventory_update(){
+                inventory_file=/var/www/html/inventory.html
+                log_type="httpd-logs"
+                timestamp=$(stat --printf=%y /tmp/$filename | cut -d.  -f1)
+                file_type=${filename##*.}
+                size=$(ls -lh /tmp/${filename} | cut -d " " -f5)
+
+
+                echo "File_Name : $filename"
+                echo "Log_Type : $log_type"
+                echo "Time_Of_Creation : $timestamp"
+                echo "Type_Of_File : $file_type"
+                echo "File_Size : $size"
+
+                if  test -f "$inventory_file"
+                then
+                        echo "<br>${log_type}&nbsp;&nbsp;&nbsp;&nbsp;${timestamp}&nbsp;&nbsp;&nbsp;&nbsp;${file_type}&nbsp;&nbsp;&nbsp;&nbsp;${size}">>"${inventory_file}"
+                        echo "Inventory file has been updated"
+                else
+                        echo "Creating '$inventory_file'"
+                       `touch ${inventory_file}`
+                        echo "<b>Log Type&nbsp;&nbsp;&nbsp;&nbsp;Date Created&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;Size</b>">>"${inventory_file}"
+                        echo "<br>${log_type}&nbsp;&nbsp;&nbsp;&nbsp;${timestamp}&nbsp;&nbsp;&nbsp;&nbsp;${file_type}&nbsp;&nbsp;&nbsp;&nbsp;${size}">>"${inventory_file}"
+                        echo "UPDATED '$inventory_file' HEADER and Data"
+                fi
+
+}
+
+
+
+#Creating Cron Job to execute automation.sh every day
+cron_file_check(){
+                cron_file=/etc/cron.d/automation
+                if test -f "$cron_file"
+                then
+                        echo "Cron Exists"
+                else
+                        echo "Creating Cron File $cron_file"
+                        touch $cron_file
+                        echo "SHELL=/bin/bash" > $cron_file
+                        echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin" >> $cron_file
+                        echo "0 0 * * * root /root/Automation_Project/automation.sh" >> $cron_file
+                        echo "Cron file created"
+                fi
+
+}
+
+
+
 ## Main Program
 my_program(){
         echo "Starting Atomation Script";
-        update_package;
 
+        update_package;
 
 
         apache_installation "$service"
@@ -93,10 +140,12 @@ my_program(){
         archive_logs
 
 
-
         copy_archive_logs "$bucketName"
+        inventory_update
+        cron_file_check
 
         echo "Automation script finished";
 }
 
 my_program;
+
